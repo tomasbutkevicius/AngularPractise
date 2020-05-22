c8f5f45b19e7d6ad5123bf18eb9e3a8afc4285b2
tomasbutkevicius <tomas.butkevicius99@gmail.com>
2020-05-22 13:49:03 +0300

    added simple type compare

---
+++ typingGame/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  randomText = lorem.sentence();
+  typedText = '';
+
+  title = 'typingGame';
+
+  onInput(value: string){
+    this.typedText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string){
+    if(!enteredLetter){
+      return 'pending';
+    }
+
+    if(enteredLetter === randomLetter){
+      return 'correct';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+
+  }
+
+}
